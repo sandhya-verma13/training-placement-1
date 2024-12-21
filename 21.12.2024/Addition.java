2b557fb47a1e28481a0cfbeaf28ff44d25879172
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2024-12-21 21:48:43 +0530

    Add files via upload

---
+++ 21.12.2024/Addition.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Addition{
+    public static void main(String args[]){
+        int x,y,sum;
+        Scanner myObj = new Scanner(System.in); 
+        System.out.println("Enter first number");
+        x = myObj.nextInt(); 
+        System.out.println("Enter second number");
+        y = myObj.nextInt(); 
+        sum = x + y; 
+        System.out.println("Sum is: " + sum); 
+    } 
+}
