b6a44d7b7ddb5b4b8218dca67bf6c2ffd1ea090e
Sandhya verma <147616301+sandhya-verma13@users.noreply.github.com>
2024-12-23 20:55:12 +0530

    Add files via upload

---
+++ 23.12.2024/StringToArray.java
@@ -0,0 +1,9 @@
+public class StringToArray {
+    public static void main(String[] args) {
+        String string = "Sandhya";
+        char[] mystring = string.toCharArray();
+        for(int i=0;i<mystring.length;i++){
+            System.out.println(mystring[i]);
+        }
+    }
+}
